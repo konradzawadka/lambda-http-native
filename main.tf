fcfad228bd608de3f8b36515a3b1aa8f4b4e8709
Konrad <konrad@soft4pm.com>
2021-03-06 15:42:42 +0100

    fix api

--- main.tf
+++ main.tf
@@ -62,6 +62,7 @@ resource "aws_apigatewayv2_route" "lambda" {
 
 
 resource "aws_apigatewayv2_integration" "lambda" {
+  api_id    = aws_apigatewayv2_api.lambda.id
   integration_type = "AWS_PROXY"
   connection_type           = "INTERNET"
   content_handling_strategy = "CONVERT_TO_TEXT"
