bdc9c2bd6daab770d62d8f6bafc1de132099aad5
Konrad <konrad@soft4pm.com>
2021-03-06 21:51:17 +0100

    payload 2.0

--- main.tf
+++ main.tf
@@ -70,10 +70,9 @@ resource "aws_apigatewayv2_stage" "lambda" {
 resource "aws_apigatewayv2_integration" "lambda" {
   api_id    = aws_apigatewayv2_api.lambda.id
   integration_type = "AWS_PROXY"
-  connection_type           = "INTERNET"
+  payload_format_version  = 2.0
   integration_method        = "POST"
   integration_uri           = aws_lambda_function.lambda.invoke_arn
-  passthrough_behavior      = "WHEN_NO_MATCH"
 }
   
 
