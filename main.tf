8fe1b221254147a51bc5788ed5c0636ff4ceb243
konradzawadka <konradzawadka@gmail.com>
2021-03-06 15:04:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,35 +46,38 @@ resource "aws_iam_role_policy_attachment" "lambda_policy-attachment" {
   policy_arn = var.policy_arn
 }
 
-resource "aws_api_gateway_rest_api" "quarkus_gateway" {
-  name = var.name
-  description = var.name
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = var.name
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "quarkus_proxy" {
-  rest_api_id = aws_api_gateway_rest_api.quarkus_gateway.id
-  parent_id = aws_api_gateway_rest_api.quarkus_gateway.root_resource_id
-  path_part = "{proxy+}"
+resource "aws_apigatewayv2_integration" "example" {
+  api_id           = aws_apigatewayv2_api.lambda.id
+  integration_type = "HTTP_PROXY"
+
+  integration_method = "ANY"
+  integration_uri    = "https://example.com/{proxy}"
+}
+
+resource "aws_apigatewayv2_route" "lambda" {
+  api_id    = aws_apigatewayv2_api.example.id
+  route_key = "ANY /example/{proxy+}"
+
+  target = "integrations/${aws_apigatewayv2_integration.example.id}"
 }
 
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.quarkus_gateway.id
-  resource_id = aws_api_gateway_resource.quarkus_proxy.id
-  http_method = "ANY"
-  authorization = "NONE"
+
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  integration_type = "AWS_PROXY"
+  connection_type           = "INTERNET"
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.example.invoke_arn
+  passthrough_behavior      = "WHEN_NO_MATCH"
 }
-
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.quarkus_gateway.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
-
-  integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.example.invoke_arn
-}
+  
 
 
 resource "aws_api_gateway_deployment" "quarkus_deployment" {
