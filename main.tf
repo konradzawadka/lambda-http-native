1c338850f61658f4f72c5c080dfc457b5c2872cf
Konrad <konrad@soft4pm.com>
2021-03-06 15:38:39 +0100

    fix arn

--- main.tf
+++ main.tf
@@ -88,5 +88,5 @@ resource "aws_lambda_permission" "apigw" {
 
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.quarkus_gateway.execution_arn}/*/*"
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
