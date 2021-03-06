efde88859a66a3e34384b50be5d0cfd81df967c3
Konrad <konrad@soft4pm.com>
2021-03-06 16:20:19 +0100

    fix content

--- main.tf
+++ main.tf
@@ -68,7 +68,6 @@ resource "aws_apigatewayv2_integration" "lambda" {
   api_id    = aws_apigatewayv2_api.lambda.id
   integration_type = "AWS_PROXY"
   connection_type           = "INTERNET"
-  content_handling_strategy = "CONVERT_TO_TEXT"
   integration_method        = "POST"
   integration_uri           = aws_lambda_function.lambda.invoke_arn
   passthrough_behavior      = "WHEN_NO_MATCH"
